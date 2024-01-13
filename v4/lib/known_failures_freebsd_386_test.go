8f038c427d0f3831b7c43398107535f64d9a3d52
Jan Mercl <0xjnml@gmail.com>
2024-01-13 12:15:30 +0100

    add known_failures_freebsd_386_test.go

---
+++ v4/lib/known_failures_freebsd_386_test.go
@@ -0,0 +1,16 @@
+// Copyright 2023 The CCGO Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package ccgo // import "modernc.org/ccgo/v4/lib"
+
+var testExecKnownFails = map[string]struct{}{
+	// EXEC FAIL - compiles and builds but fails when executed.
+
+
+	// BUILD FAIL - compiles but does not build.
+
+
+	// COMPILE FAIL - does not compile.
+
+}
